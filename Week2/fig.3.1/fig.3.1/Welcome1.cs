ebd0b35c75c96c4da0856a382585dbac5e4741c1
April Graves <gravesae@seminolestate.edu>
2015-09-01 10:49:39 -0400

    Added fig.3.1

---
+++ Week2/fig.3.1/fig.3.1/Welcome1.cs
@@ -0,0 +1,16 @@
+﻿//Fig. 3.1: Welcome1.cs
+//Text-displaying app.
+using System;
+
+ public class Welcome1
+ {
+   //main method begins execution of C# app
+   static void Main(string[] args)
+   {
+     Console.WriteLine("Welcome to C# Programming!");
+     Console.Read();
+   } //end Main
+ } //end class Welcome1
+
+ /* This is a delimited comment.
+     It can be split over many lines */
